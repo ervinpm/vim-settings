984185c01446beb0cd356bb85778b98754b29645
Ervin Marasigan <ervinpm@gmail.com>
2024-02-23 21:04:13 -0600

    fix: added undodir ignore

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.5', 
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+        require('telescope').setup{
+            defaults = {
+            file_ignore_patterns = { "undodir/" }
+            }
+        }
+    end
 }
