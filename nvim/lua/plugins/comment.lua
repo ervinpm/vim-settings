350857395360653143f93233b83c2d429d728e58
Ervin Marasigan <ervin.marasigan@delta.com>
2024-02-28 18:32:26 -0600

    fix: buffer mapping conflicts

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,7 +1,12 @@
 return {
-    'numToStr/Comment.nvim',
+
+    'ervinpm/Comment.nvim',
     config = function ()
-        require('Comment').setup()
-    end
+        require('Comment').setup({
+            mappings = {
+                block = false
+            },
+        })
+    end,
 }
 
