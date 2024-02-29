188bf5ce53dd2ed1a0a3b80c1ecf880986c5b159
Ervin Marasigan <ervinpm@gmail.com>
2024-02-29 06:59:21 -0600

    feat: add buffer line

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,14 +4,14 @@ return {
     config = function()
         require('lualine').setup { 
             options = { theme = "catppuccin" },
-            tabline = {
-              lualine_a = {'buffers'},
-              lualine_b = {},
-              lualine_c = {},
-              lualine_x = {},
-              lualine_y = {},
-              lualine_z = {'tabs'}
-            },
+            -- tabline = {
+            --   lualine_a = {'buffers'},
+            --   lualine_b = {},
+            --   lualine_c = {},
+            --   lualine_x = {},
+            --   lualine_y = {},
+            --   lualine_z = {'tabs'}
+            -- },
         }
     end
 }
